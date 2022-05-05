2002231a29db1d5662cd8d4b9915b81fc3559ccc
root <rooknpown@naver.com>
2022-05-05 08:19:01 +0000

    implementing submesh

--- mesh.py
+++ mesh.py
@@ -1,19 +1,23 @@
 import os
 import numpy as np
+import torch
 
 class Mesh():
 
-    def __init__(self, file):
+    def __init__(self, file, device):
 
         if not os.path.exists(file):
             print("Mesh file does not exist at: " + str(file))
             return
 
+        self.device = device
     
         self.edges = None
 
-        self.vertices, self.faces = load_obj(file)
+        self.vertices, self.faces = self.load_obj(file)
         self.normalize()
+        self.vertices = torch.from_numpy(self.vertices).to(self.device)
+        self.faces = torch.from_numpy(self.faces).to(self.device)
         
         self.v_mask = np.ones(len(self.vertices))
 
@@ -54,19 +58,110 @@ class Mesh():
         xyzScale = []
         self.translation = []
         for i in range(3):
-            maxCoord.append(self.vs[:,i].max())
-            minCoord.append(self.vs[:,i].min())
+            maxCoord.append(self.vertices[:,i].max())
+            minCoord.append(self.vertices[:,i].min())
             xyzScale.append(maxCoord[i] - minCoord[i])
-        scale = max(xyzScale)
+        self.scale = max(xyzScale)
         for i in range(3):
-            self.translation.append((-maxCoord[i] - minCoord[i])/2/scale)
+            self.translation.append((-maxCoord[i] - minCoord[i])/2/self.scale)
 
 
         self.vertices /= self.scale
-        self.vertices += self.translation[None, :]
-
+        self.vertices += [self.translation]
         
 
 
-class PartMesh():
-    def __init__(self):
+class SubMesh():
+    def __init__(self, base_mesh: Mesh, sub_num = 1, bfs_depth = 0):
+
+        self.base_mesh = base_mesh
+        self.subvertices, self.num_sub = self.set_shape(self.base_mesh.vertices, sub_num)
+
+        self.sub_mesh = []
+        self.sub_mesh_idx = []
+        self.init_vertices = []
+
+        self.get_submesh(bfs_depth)
+
+
+
+    def set_shape(self, vertices: torch.Tensor, sub_num):
+        
+        center = vertices.mean(dim=0)
+
+        diff_vertices = vertices - center
+        diff_vertices = diff_vertices + diff_vertices
+        sub = torch.zeros(vertices.shape[0]).long().to(diff_vertices.device)
+        if sub_num >= 2:
+            sub += 1*(diff_vertices[:,0]>0).long()
+        if sub_num >= 4:
+            sub += 2*(diff_vertices[:,1]>0).long()
+        if sub_num >= 8:
+            sub += 4*(diff_vertices[:,2]>0).long()
+
+        num_sub = torch.max(sub).item() + 1
+
+        return sub, num_sub
+
+    def get_submesh(self, bfs_depth):
+        
+        for i in range(self.num_sub):
+            idx = torch.nonzero((self.subvertices == i), as_tuple=False)
+            print(idx.shape)
+            idx = idx.squeeze(1)
+            if idx.size()[0] == 0:
+                continue
+            idx = torch.sort(self.subvertices, dim = 0)[0]
+            idx = self.bfs(idx, self.base_mesh.faces, bfs_depth).type(idx.dtype).clone().detach().to(idx.device)
+            self.create_submesh(idx)
+
+    def bfs(self, idxs, faces, bfs_depth):
+        if bfs_depth <= 0:
+            return idxs
+        
+        idxs = idxs.tolist()
+        faces = faces.tolist()
+
+        marked = []
+        for vertice in idxs:
+            marked.append((vertice, 0))
+        visited = idxs
+        while len(marked) >0:
+            idx, depth = marked.pop()
+            for f in faces:
+                if idx in f:
+                    for j in f:
+                        if j not in visited:
+                            if depth + 1 <= bfs_depth:
+                                marked.put((j, depth + 1))
+                            visited.append(j)
+        visited = sorted(visited)
+        return visited
+
+    def create_submesh(self, idx):
+
+        mesh = self.base_mesh
+        mask = torch.zeros(len(mesh.vertices))
+        mask[idx] = 1
+        facemask = mask[mesh.faces].sum(dim = -1) > 0 
+        newfaces = mesh.faces[facemask].clone()
+
+        totvertices = newfaces.reshape(-1)       
+
+        mask2 = torch.zeros(len(mesh.vertices)).long().to(totvertices.device)
+        mask2[totvertices] = 1
+
+        idx2 = self.mask2index(mask2)
+        vertices = mesh.vertices[idx2, :].clone()
+        
+        mask3 = torch.zeros(len(mesh.vertices))
+        mask3[idx2] = 1
+        cumsum = torch.cumsum(1 - mask3, dim = 0)
+        
+        
+    def mask2index(self, mask):
+        idx = []
+        for i, j in enumerate(mask):
+            if j == 1:
+                idx.append(i)
+        return torch.tensor(idx).type(torch.long)
--- run.py
+++ run.py
@@ -1,11 +1,62 @@
 import hydra
-from mesh import Mesh
+import numpy as np
+import torch
+from mesh import Mesh, SubMesh
 
 @hydra.main(config_path=".", config_name="run.yaml")
 def main(config):
-    mesh = Mesh()
-    
+    pcpath = config.get("pcpath")
+    initmesh = config.get("initmeshpath")
+    savepath = config.get("savepath")
+    bfs_depth = config.get("bfs_depth")
 
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+    else:
+        device = torch.device('cpu')
+
+    mesh = Mesh(initmesh, device)
+    coords, normals = read_pcs(pcpath)
+
+    # normalize coordinates and normals
+    coords = normalize(coords, mesh.scale, mesh.translation)
+    normals = np.array(normals, dtype=np.float32)
+
+    # give to gpu
+    coords = torch.Tensor([coords]).type(torch.float32).to(device)
+    normals = torch.Tensor([normals]).type(torch.float32).to(device)
+
+    num_submesh = get_num_submesh(len(mesh.faces))
+    sub_mesh = SubMesh(mesh, num_submesh, bfs_depth = bfs_depth)
+
+
+def read_pcs(file):
+
+    coords = []
+    normals = []
+    f = open(file)
+    for line in f:
+        line = line.strip().split()
+        if(len(line) == 6):
+            coord = [line[0],line[1],line[2]]
+            coords.append(coord[:])
+            normal = [line[3],line[4],line[5]]
+            normals.append(normal[:])
+
+    return coords, normals
+
+def normalize(coords, scale, translation):
+    coords = np.array(coords, dtype=np.float32)
+    coords /= scale
+    coords += [translation]
+
+    return coords
+
+def get_num_submesh(num_faces):
+    subnumList = [1, 2, 4, 8]
+    faceBin = [8000, 16000, 20000]
+    num_submesh = subnumList[np.digitize(num_faces, faceBin)]
+    return num_submesh
 
 if __name__ == '__main__':
     main()
