3dff5ab2fb2ab5225ab6e948f0c3cee1379c0c0f
root <rooknpown@naver.com>
2022-05-25 06:37:52 +0000

    added alpha shape

--- dataprocessing/initial_mesh_generation.py
+++ dataprocessing/initial_mesh_generation.py
@@ -7,15 +7,26 @@ def out_path_type(out_path):
     return out_path
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description="Generate Initial Mesh", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument("--pcd_path", required = True, type= str, dest="pcd_path")
-  parser.add_argument("--out_path", required = True, type= out_path_type, dest="out_path")
+    parser = argparse.ArgumentParser(description="Generate Initial Mesh", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--pcd_path", required = True, type= str, dest="pcd_path")
+    parser.add_argument("--out_path", required = True, type= out_path_type, dest="out_path")
+    parser.add_argument("--genus", required = False, type= int, dest="genus")
+    args = parser.parse_args()
 
-  args = parser.parse_args()
+    pcd_path = args.pcd_path
+    out_path = args.out_path
+    genus = args.genus
 
-  pcd_path = args.pcd_path
-  out_path = args.out_path
+    if genus is None or genus > 0:
+        isgenus0 = False
+    else:
+        isgenus0 = True
 
-  pointCld = open3d.io.read_point_cloud(pcd_path)
-  hull, _ = pointCld.compute_convex_hull()
-  open3d.io.write_triangle_mesh(out_path, hull)
+    pointCld = open3d.io.read_point_cloud(pcd_path)
+    if isgenus0 :
+        hull, _ = pointCld.compute_convex_hull()
+        open3d.io.write_triangle_mesh(out_path, hull)
+    else:
+        alpha = 0.5
+        mesh = open3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pointCld, alpha)
+        open3d.io.write_triangle_mesh(out_path, mesh)
